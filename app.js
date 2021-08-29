29d3adee4956e2bae11642e3e6e4a1db86871e1d
Jada Lilleboe <lilleboejada@gmail.com>
2021-08-29 09:07:13 -0500

    initial

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const express = require('express')
+const app = express()
 
 let users = [
   {
@@ -16,3 +18,35 @@ let users = [
     favoriteSubject: "Science"
   }
 ]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World </h1>')
+})
+
+app.get('/users', (request, response) => {
+  response.json(users)
+})
+
+app.get('/users/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const user = users.find(user => user.id === id)
+
+  response.json(user)
+})
+
+app.delete('/users/:id', (request, response) => {
+  const id = Number(request.params.id)
+  users = users.filter(user => user.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/users', (request, response) => {
+  const user = request.body
+  user.id = 4
+
+  users = users.concat(user)
+  response.json(user)
+})
+
+module.exports = app
