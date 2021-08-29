615d58c3dd122b7aeea3270f178b73dc12b08b07
Jada Lilleboe <lilleboejada@gmail.com>
2021-08-29 07:29:44 -0500

    add example data

---
+++ app.js
@@ -0,0 +1,18 @@
+
+let users = [
+  {
+    id: 1,
+    name: "Ada Lovelace",
+    favoriteSubject: "Science"
+  },
+  {
+    id: 2,
+    name: "Leonhard Euler",
+    favoriteSubject: "Math"
+  },
+  {
+    id: 3,
+    name: "Isaac Newton",
+    favoriteSubject: "Science"
+  }
+]
