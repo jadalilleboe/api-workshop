5d210382e616f656f00ff6d91bf06b746fa25730
Jada Lilleboe <lilleboejada@gmail.com>
2021-08-29 07:07:30 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const app = require('./app')
+const http = require('http')
+const logger = require('./utils/logger')
+
+const server = http.createServer(app)
+
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+  logger.info(`http://localhost:${config.PORT}/api/blogs`)
+})
